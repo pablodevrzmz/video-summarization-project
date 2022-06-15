8ffa30476d2cd33d9091d3850e2dd821a9384332
Jose Pablo Ramírez Méndez <pablo.dev.rz.mz@gmail.com>
2022-06-14 21:09:00 -0600

    Cuda and stages 1,2 and 3 working with small videos

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 
 
 '''
+Dataset summary
 {'Air_Force_One.mp4': 4494, 'Base jumping.mp4': 4729, 'Bearpark_climbing.mp4': 3341, 'Bike Polo.mp4': 3064, 'Bus_in_Rock_Tunnel.mp4': 5131, 'car_over_camera.mp4': 4382, 'Car_railcrossing.mp4': 5075, 'Cockpit_Landing.mp4': 9046, 'Cooking.mp4': 1280, 'Eiffel Tower.mp4': 4971, 'Excavators river crossing.mp4': 9721, 'Fire Domino.mp4': 1612, 'Jumps.mp4': 950, 'Kids_playing_in_leaves.mp4': 3187, 'Notre_Dame.mp4': 4608, 'Paintball.mp4': 6096, 'paluma_jump.mp4': 2574, 'playing_ball.mp4': 3119, 'Playing_on_water_slide.mp4': 3065, 'Saving dolphines.mp4': 6683, 'Scuba.mp4': 2221, 'St Maarten Landing.mp4': 1751, 'Statue of Liberty.mp4': 3863, 'Uncut_Evening_Flight.mp4': 9672, 'Valparaiso_Downhill.mp4': 5178}
 '''
 
@@ -54,8 +55,18 @@ def split_dataset(dataset_path):
 
     return frames_counter
 
-if __name__ == "__main__":
+def run_video_sumarization(frames_path):
+    main_featues = extract_features(frames_path)
+    print(f"Features summary: final array shape: {main_featues[0].shape}, used frames: {len(main_featues[1])}")
 
+def generate_dataset():
     frames_per_video = split_dataset("C:\\Users\\XPC\\Downloads\\SumMe\\videos\\")
     print(frames_per_video)
-    #extract_features('./frames_dataset/monkeys/')
+
+if __name__ == "__main__":
+
+    # Preprocessing steps
+    generate_dataset()
+
+    # Summarize video frames
+    run_video_sumarization(f"./frames_dataset/SumMe/Jumps/")
--- model/model.py
+++ model/model.py
@@ -3,37 +3,61 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.utils import image_dataset_from_directory
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.models import Model
-from sklearn_extra.cluster import KMedoids
 import numpy as np
 import os
 
-def get_frames_len(dir_path):
+def __print_cuda_summary():
+    import tensorflow as tf
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+    print("Num CPUs Available: ", len(tf.config.list_physical_devices('CPU')))
+    print("Cuda Availability: ", tf.test.is_built_with_cuda())
+
+def __get_frames_len(dir_path):
     _, _, files = next(os.walk(dir_path))
     return len(files)
 
+def __chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
 #https://towardsdatascience.com/extract-features-visualize-filters-and-feature-maps-in-vgg16-and-vgg19-cnn-models-d2da6333edd0
 # image shape=[samples, rows, cols, channels]
+
 def extract_features(image_dir_path):
+
+    __print_cuda_summary()
+    
+    files = os.listdir(image_dir_path)
+    frames_count = len(files)
+
+    print(f"Step 1: Processing {frames_count} frames from path {image_dir_path}")
+
     vgg = VGG16(weights='imagenet', include_top=False)
     model = Flatten(name="flatten") (vgg.output)
     model = Model(inputs=vgg.input, outputs=model)
-    #frame_count= get_frames_len(image_dir_path)
+
     model.summary()
+    
     X = list()
-    for i in range(0, 1000):
-        #print(i)
-        img_path=image_dir_path+str(i)+'.jpg'
-        img = image.load_img(img_path)
-        loaded_img = image.img_to_array(img)
-        img_dims = np.expand_dims(loaded_img, axis=0)
-        X.append(img_dims)
+    X_Control = list()
+
+    frame_chunks = __chunks(files,25)
+
+    for j,chunck in enumerate(frame_chunks):
+        print(f"\tReading chunk {j+1}")
+        for i in range(0, 5):
+            if i*5 < len(chunck):
+                img_path=image_dir_path+chunck[i*5]
+                img = image.load_img(img_path)
+                loaded_img = image.img_to_array(img)
+                img_dims = np.expand_dims(loaded_img, axis=0)
+                X.append(img_dims)
+                X_Control.append(chunck[i*5])
+    
+    print("Step 2: Images ready as numpy arrays")
     X = np.array(X).squeeze() #Removes the 'samples' part of the shape from each image, which is of shape=1, and the new amount of sample will be the first array, containing the amount of images.
-    #print(X.shape)
-    #print(model.output)
     X = preprocess_input(X)
 
-    block4_pool_features = model.predict(X)
-    kmedoids = KMedoids(n_clusters=2, random_state=0).fit(block4_pool_features)
-    print(kmedoids.labels_)
-    #print(block4_pool_features.shape)
-    #print(block4_pool_features)
+    print("Step 3: Extracting features")
+    return model.predict(X), X_Control
